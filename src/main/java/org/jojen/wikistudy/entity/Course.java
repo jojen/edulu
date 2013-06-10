d3e124a3cff4e86dd7af8ace567f39132f7b7eb6
jojen <m@jojen.de>
2013-06-10 12:02:17 +0200

    Unlimited Text

--- src/main/java/org/jojen/wikistudy/entity/Course.java
+++ src/main/java/org/jojen/wikistudy/entity/Course.java
@@ -1,9 +1,6 @@
 package org.jojen.wikistudy.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -14,80 +11,80 @@ import java.util.List;
 
 @Entity
 public class Course implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue
-	private Integer id;
-
-
-	@Size(min = 1, max = 50)
-	@NotNull
-	private String name;
+    @Id
+    @GeneratedValue
+    private Integer id;
 
 
-	@Min(1)
-	@Max(200)
-	private Integer age;
+    @Size(min = 1, max = 50)
+    @NotNull
+    private String name;
 
 
-	private String description;
+    @Min(1)
+    @Max(200)
+    private Integer age;
 
-	@OneToMany
-	private List<Lesson> lessons;
+    @Column(columnDefinition = "TEXT")
+    private String description;
 
-	public Integer getId() {
-		return id;
-	}
+    @OneToMany
+    private List<Lesson> lessons;
 
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Boolean getHasDraft() {
-		return true;
-	}
-
-	public List<Lesson> getLessons() {
-		if (lessons == null) {
-		   lessons = new ArrayList();
-		}
-		return lessons;
-	}
-
-	public void addLessons(Lesson lesson) {
-		if (lessons == null) {
-			lessons = new ArrayList<Lesson>();
-		}
-		if (lesson != null) {
-			lessons.add(lesson);
-		}
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+    public Integer getId() {
+        return id;
+    }
 
 
-	@Override
-	public String toString() {
-		return "Course [id=" + id + ", name=" + name + "]";
-	}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getHasDraft() {
+        return true;
+    }
+
+    public List<Lesson> getLessons() {
+        if (lessons == null) {
+            lessons = new ArrayList();
+        }
+        return lessons;
+    }
+
+    public void addLessons(Lesson lesson) {
+        if (lessons == null) {
+            lessons = new ArrayList<Lesson>();
+        }
+        if (lesson != null) {
+            lessons.add(lesson);
+        }
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", name=" + name + "]";
+    }
 }
--- src/main/java/org/jojen/wikistudy/entity/Text.java
+++ src/main/java/org/jojen/wikistudy/entity/Text.java
@@ -1,14 +1,19 @@
 package org.jojen.wikistudy.entity;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
 public class Text extends Content implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Size(max = 50)
     private String name;
+
+    @Column(columnDefinition = "TEXT")
     private String text;
 
     public String getName() {
